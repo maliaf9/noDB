1f1e239e92f8abd7e7ff626d7dd95e4b82550ebd
Malia Franks <malia_franks@yahoo.com>
2019-07-09 13:30:30 -0700

    myproj

---
+++ src/component/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FullMoon from '../../images/full-moon.jpg';
+
+
+const home = props => (
+     <div>
+        <img className= 'moon' src={FullMoon} alt={props}/>
+        
+      </div>
+);
+
+
+export default home
